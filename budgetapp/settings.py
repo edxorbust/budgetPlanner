00c79087d1d983c577541e3ed77b67a297da6e03
Eduardo Orbe <eduardoorbeb@gmail.com>
2023-11-15 22:03:25 -0800

    csrf corrected

--- budgetapp/settings.py
+++ budgetapp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-2f17.up.railway.app']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
